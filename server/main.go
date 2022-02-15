ee7d73ec3f900b77c88e7bc4e71224473ace144b
Hasan Yousef <hyousef@msn.com>
2022-02-15 17:29:04 +0300

    cleaning

--- server/main.go
+++ server/main.go
@@ -18,11 +18,11 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-	s := chat.Server{}
+	s := chat.Server{} // why not defined?
 
 	grpcServer := grpc.NewServer()
 
-	chat.RegisterChatServiceServer(grpcServer, &s)
+	chat.RegisterChatServiceServer(grpcServer, &s) // why not defined?
 
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %s", err)
